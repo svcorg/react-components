876613eb32af5b059f9d18d5f980594a313c8ecd
Chaitanya <kamthamc@users.noreply.github.com>
2020-04-04 23:48:48 -0700

    break: multiple outputs format support and minification
    BREAKING CHANGE
    
    Supporting es(default dist/index.js) cjs, amd, umd, system, iife
    and minification

--- packages/copy-to-clipboard/rollup.config.js
+++ packages/copy-to-clipboard/rollup.config.js
@@ -1,12 +1,51 @@
-import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
 
 export default {
   input: [
     'lib/index.ts'
   ],
-  output: {
-    dir: 'dist',
-    format: 'es'
-  },
-  plugins: [typescript()]
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      name: 'copyToClipboard'
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'copyToClipboard',
+      globals: {
+        react: 'React'
+      }
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      name: 'copyToClipboard'
+    },
+    {
+      file: 'dist/index.iife.js',
+      format: 'iife',
+      name: 'copyToClipboard',
+      globals: {
+        react: 'React'
+      }
+    },
+    {
+      file: 'dist/index.system.js',
+      format: 'system',
+      name: 'copyToClipboard'
+    },
+    {
+      file: 'dist/index.amd.js',
+      format: 'amd',
+      name: 'copyToClipboard'
+    }
+  ],
+  plugins: [
+    terser(), 
+    typescript()
+  ],
+  external: ['react']
 };
